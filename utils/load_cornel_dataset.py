b70a499360e025a92fddb4971e9a5049732ae5ea
Plaif_vision_park <chansoo0710@gmail.com>
2022-01-19 18:39:42 +0900

    Update: load_cornell_dataset

--- utils/load_cornel_dataset.py
+++ utils/load_cornel_dataset.py
@@ -1,9 +1,11 @@
 import glob
+import imp
 import os
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from imageio import imread
+from imageio import imread, imsave
 import numpy as np
+from tqdm import tqdm
 
 
 class CornellDataset:
@@ -20,10 +22,26 @@ class CornellDataset:
 
         self.depth_files = [f.replace('cpos.txt', 'd.tiff') for f in self.grasp_files] 
         self.rgb_files = [f.replace('d.tiff', 'r.png') for f in self.depth_files]
+        
+output_path = './cornell_output/'
+rgb_path = output_path + 'rgb/'
+depth_path = output_path + 'depth/'
+os.makedirs(output_path, exist_ok=True)
+os.makedirs(rgb_path, exist_ok=True)
+os.makedirs(depth_path, exist_ok=True)
 
 dataset = CornellDataset(file_path='./datasets/')
-len = dataset.length
-print(len)
-depth_test = imread(dataset.depth_files[0])
+# len = dataset.length
+# print(len)
+# depth_test = imread(dataset.depth_files[0])
+
+pbar = tqdm(range(dataset.length))
+
+for i in pbar:
+    rgb = imread(dataset.rgb_files[i])
+    imsave(rgb_path + str(i) + '_rgb.jpeg', rgb)
+    depth = imread(dataset.depth_files[i])
+    imsave(depth_path + str(i) + '_depth.tiff', depth)
+
 
 
